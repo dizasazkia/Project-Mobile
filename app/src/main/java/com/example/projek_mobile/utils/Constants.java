466a73f2a6b404f95b6bf6528bf41720056156f2
dizasazkia <144410116+dizasazkia@users.noreply.github.com>
2025-06-10 22:26:58 +0800

    feat: Define Constants for news API base URL and key

---
+++ app/src/main/java/com/example/projek_mobile/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.projek_mobile.utils;
+
+import com.example.projek_mobile.BuildConfig;
+
+public class Constants {
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static final String API_KEY = BuildConfig.NEWS_API_KEY;
+}
